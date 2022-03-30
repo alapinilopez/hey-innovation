4253d7ce830dd93135391704448eeba836e5df76
alapinilopez <alapinilopez@cifpfbmoll.eu>
2022-03-30 12:05:38 +0200

    feat: :sparkles: tratamiento de campos

--- Formacion_Front/HTML-CSS-JS/javascript/default.js
+++ Formacion_Front/HTML-CSS-JS/javascript/default.js
@@ -1,3 +1,24 @@
 function newWindow() {
-    window.open("/hey-innovation/Formacion_Front/HTML-CSS-JS/holaMundo.html", "targetWindow","height=320px, width=150px")
+    window.open("/hey-innovation/Formacion_Front/HTML-CSS-JS/holaMundo.html", "targetWindow", "height=320px, width=150px")
+}
+
+function validateEmail() {
+    let email = document.getElementById("email").value;
+    var emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+        if (!(emailRegex.test(email))) {
+            return document.getElementsByClassName("displayError").style.display = "block";
+        }else {
+            return true;
+        }
+}
+
+
+function validatePwd()
+let pwd = document.getElementById("pwd").value;
+var pwdRegex = /^[A-Za-z]\w{4,}$(?=.*\d)/;
+
+if(pwd.value.match(pwdRegex)) {
+    alert("Contraseña aceptada")
+}else {
+    document.getElementsByClassName("displayError").style.display = "block";
 }
