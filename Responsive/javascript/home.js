81a7a76acfee9cf156fc0334b9092d0b6174800b
alapinilopez <alapinilopez@cifpfbmoll.eu>
2022-05-09 13:54:09 +0200

    feat: :sparkles: Images now clickable

---
+++ Responsive/javascript/home.js
@@ -0,0 +1,26 @@
+function messaging() {
+    let panel = document.getElementById("messaging");
+    if (panel.style.display == "none") {
+        panel.style.display = "block";
+    } else {
+        panel.style.display = "none";
+    }
+}
+
+function calendar() {
+    let panel = document.getElementById("calendar");
+    if (panel.style.display == "none") {
+        panel.style.display = "block";
+    } else {
+        panel.style.display = "none";
+    }
+}
+
+function care() {
+    let panel = document.getElementById("care");
+    if (panel.style.display == "none") {
+        panel.style.display = "block";
+    } else {
+        panel.style.display = "none";
+    }
+}
