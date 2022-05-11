3e550ceddddcd730e416fe1d21be1e852d16b5ec
alapinilopez <alapinilopez@cifpfbmoll.eu>
2022-05-11 13:00:54 +0200

    feat: :zap: 𝔁𝓾𝓮🥶𝓱𝓾𝓪🧚‍♀️𝓹𝓲𝓪𝓸😻𝓹𝓲𝓪𝓸🗿𝓫𝓮𝓲👺𝓯𝓮𝓷𝓰🤩𝔁𝓲𝓪𝓸😼𝔁𝓲𝓪𝓸👣

--- Responsive/javascript/home.js
+++ Responsive/javascript/home.js
@@ -1,5 +1,12 @@
 function messaging() {
-    let panel = document.getElementById("messaging");
+    let bar = document.getElementById("messaging");
+    if (bar.style.display == "none") {
+        bar.style.display = "block";
+    } else {
+        bar.style.display = "none";
+    }
+
+    let panel = document.getElementById("messaging-panel");
     if (panel.style.display == "none") {
         panel.style.display = "block";
     } else {
@@ -23,4 +30,21 @@ function care() {
     } else {
         panel.style.display = "none";
     }
+}
+
+// CAMBIAR IMAGEN SEGUN EL MEDIA QUERY
+function mobileImage(mobileSize) {
+    let messageBar = document.getElementById("messaging");
+    let messagePanel = document.getElementById("messaging-panel");
+    
+    if (mobileSize.matches) {
+        messageBar.src = "Responsive/images/Barra_Messaging_Smartphone.png"
+        messagePanel.src = "Responsive/images/messaging_panel_smartphone.png"
+    } else {
+        messagePanel.src = "/Responsive/images/Mensajes.png"
+    }
+
+    var mobileSize = window.matchMedia("(max-width: 640px)");
+    care(mobileSize);
+    mobileSize.addListener(care);
 }
